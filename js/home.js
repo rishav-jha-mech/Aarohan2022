1aed2fced5e7f2431db2e3589bd243bdd7948367
rishav-jha-mech <rishav.rj74@gmail.com>
2022-02-20 16:49:31 +0530

    First stable mobile version

--- js/home.js
+++ js/home.js
@@ -1,11 +1,26 @@
 const VIDEO = document.getElementById('video');
+const offsetCloseBtnHidden = document.getElementById('hiddenBtn');
+const offsetShowBtnHidden = document.getElementById('shownBtn');
 
 VIDEO.onloadeddata = function() {
     setTimeout(() => {
         loadend();
     }, 2000);
 };
-
+function closeAfterSometimePC() {
+    document.getElementById('pcham').classList.toggle('active')
+    setTimeout(() => {
+        offsetCloseBtnHidden.click()
+        document.getElementById('pcham').classList.toggle('active')
+    }, 800);
+}
+function closeAfterSometimeMob(){
+    document.getElementById('ocanva').classList.toggle('active')
+    setTimeout(() => {
+        offsetShowBtnHidden.click()
+        document.getElementById('ocanva').classList.toggle('active')
+    }, 400);
+}
 function loadend() {
     document.getElementById('loading').style.display= "none";
     document.getElementById('root').style.display= "block";
